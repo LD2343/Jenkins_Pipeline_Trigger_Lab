d04a80b89232029fe30b53b97c63d2c5eae26702
Logan T <178672247+LD2343@users.noreply.github.com>
2026-03-28 14:15:31 -0500

    updated main.tf

--- 01-main.tf
+++ 01-main.tf
@@ -2,24 +2,41 @@ resource "aws_s3_bucket" "Jenkins_Bucket" {
   bucket_prefix = "jenkins-bucket-"
   force_destroy = true
 
-
   tags = {
     Name = "Jenkins Bucket"
   }
 }
 
-resource "aws_s3_object" "Armageddon_Evidence" {
-  for_each = fileset("${path.module}/armageddon_evidence_files", "**")
-
+resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket = aws_s3_bucket.Jenkins_Bucket.id
-  key    = "armageddon_evidence_files/${each.value}"
-  source = "${path.module}/armageddon_evidence_files/${each.value}"
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+
+}
+
+
+resource "aws_s3_object" "Armageddon_Evidence_Link" {
+  bucket       = aws_s3_bucket.Jenkins_Bucket.id
+  key          = "armageddon_evidence_files/Armageddon_Group_Link.txt"
+  source       = "${path.module}/armageddon_evidence_files/Armageddon_Group_Link.txt"
+  content_type = "text/plain"
+}
+
+resource "aws_s3_object" "Armageddon_Evidence_Approval" {
+  bucket       = aws_s3_bucket.Jenkins_Bucket.id
+  key          = "armageddon_evidence_files/Theo_Armageddon_Approval.png"
+  source       = "${path.module}/armageddon_evidence_files/Theo_Armageddon_Approval.png"
+  content_type = "image/png"
 }
 
 resource "aws_s3_object" "Lab_Evidence" {
   for_each = fileset("${path.module}/lab_evidence", "**")
 
-  bucket = aws_s3_bucket.Jenkins_Bucket.id
-  key    = "lab_evidence/${each.value}"
-  source = "${path.module}/lab_evidence/${each.value}"
+  bucket       = aws_s3_bucket.Jenkins_Bucket.id
+  key          = "lab_evidence/${each.value}"
+  source       = "${path.module}/lab_evidence/${each.value}"
+  content_type = "image/png"
 }
